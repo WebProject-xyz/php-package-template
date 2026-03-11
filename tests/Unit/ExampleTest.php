f29ecf893debaf221c8a536ac41fff9f851c581a
Benjamin Fahl <git@fahl-design.de>
2026-03-11 19:41:06 +0100

    feat: Add GitHub workflow, issue templates, PR template, and initial project setup

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace WebProject\PhpPackageTemplate\Tests\Unit;
+
+use WebProject\PhpPackageTemplate\Tests\Support\UnitTester;
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+
+    protected UnitTester $tester;
+
+    protected function _before()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
